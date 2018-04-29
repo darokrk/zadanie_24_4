4d78af326da69685c9c88598f8d7b2efa09f5bc8
Dariusz Rosiek <daro.krk@interia.pl>
2018-04-29 21:19:02 +0200

    Change formatDate

--- index.js
+++ index.js
@@ -1,5 +1,34 @@
 const formatDate = (timeInSeconds) => {
-    return `${timeInSeconds}s`;
+
+    const hours = Math.floor(timeInSeconds/ 3600);
+    const minutes = Math.floor((timeInSeconds % 3600) / 60);
+    const seconds = Math.floor((timeInSeconds % 3600) % 60);
+
+    if (typeof timeInSeconds !== "undefined") {
+        if (timeInSeconds < 60) {
+            return `${seconds}s`;
+        }
+
+        if (timeInSeconds >= 60 && timeInSeconds < 3600) {
+            if (seconds != 0) {
+                return `${minuts}m ${seconds}s`;
+            } else {
+                return `${minutes}m`;
+            }
+        }
+
+        if (timeInSeconds >= 3600) {
+            if  (minutes === 0 && seconds === 0) {
+                return `${hours}h`;
+            } else if (seconds === 0) {
+                return `${hours}h ${minutes}m`;
+            } else if (minutes === 0) {
+                return `${hours}h ${seconds}s`;
+            } else {
+                return `${hours}h ${minutes}m ${seconds}s`;
+            }
+        }
+    } else return '0s';
 }
 
 module.exports = formatDate;
